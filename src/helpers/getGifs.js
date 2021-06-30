28dd6892b4ebf37f2a184eb8b7dec31fa28ccade
Fabián Araya <f_araya27@hotmail.com>
2021-06-29 20:39:20 -0600

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async(category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ZiLk7j2th6B0Ox4wXhjpZBdm0v8Se23Q`;
+    const resp = await fetch(url);
+    const {data} = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    return gifs;
+}
